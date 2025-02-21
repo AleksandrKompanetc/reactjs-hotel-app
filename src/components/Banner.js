94a306de62803fb6e53ca8ea89ce7c17f71a10e8
AleksandrKompanetc <moneymacker7@gmail.com>
2025-02-21 23:06:15 +0200

    added content for component Banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,7 +3,18 @@ import './Banner.css'
 
 function Banner() {
   return (
-    <div></div>
+    <>
+      <div className='banner'>
+        <div className='banner__info'>
+          <h4>Enjoy Your Vacation With Our Hotels</h4>
+          <h1>Let us plan you a perfect Holiday</h1>
+          <button>Explore Now</button>
+        </div>
+      </div>
+      <div className="booking__div">
+
+      </div>
+    </>
   )
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import './Home.css'
+import Banner from './Banner'
 
 function Home() {
   return (
-    <div className='home'>Home</div>
+    <div className='home'>
+      <Banner />
+    </div>
   )
 }
 
