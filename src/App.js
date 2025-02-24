544d6229985187e2559f4258cb9833073906d641
AleksandrKompanetc <moneymacker7@gmail.com>
2025-02-24 23:13:55 +0200

    added state

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [searchParams, setSearchParams] = useState({
+    checkIn: '',
+    checkOut: '',
+    guests: 1
+  })
+  
   return (
     <div className="app">
       
