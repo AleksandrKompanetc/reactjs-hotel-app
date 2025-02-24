6defbea072745eaed6798ac5a648ab7c396c382b
AleksandrKompanetc <moneymacker7@gmail.com>
2025-02-24 23:25:45 +0200

    changed component Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <nav className='navbar'></nav>
+    <nav className='navbar'>
+      <div className='nav-content'>
+        <h1>HotelBooking</h1>
+        <div className='nav-links'>
+          <a href="#home">Home</a>
+          <a href="#bookings">My bookings</a>
+          <a href="#account">Account</a>
+        </div>
+      </div>
+    </nav>
   )
 }
 
