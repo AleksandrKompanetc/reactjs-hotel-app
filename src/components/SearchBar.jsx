163c9d312c484c60bf00170af5ae06b5e01d3f65
AleksandrKompanetc <moneymacker7@gmail.com>
2025-02-25 16:12:58 +0200

    added component SearchBar

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function SearchBar() {
+
+  return (
+    <div className='search-bar'>
+      <div className='search-inputs'>
+        <div className='input-group'>
+          
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default SearchBar
