608d96b3e260fe2a7e4c8249b3889dbe8b7bb86f
AleksandrKompanetc <moneymacker7@gmail.com>
2025-02-26 22:07:57 +0200

    added component Filters

---
+++ src/components/Filters.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Filters() {
+  return (
+    <div className='filters'>
+      <div className='filter-group'>
+        <label htmlFor="">Price</label>
+        <select name="" id="">
+          
+        </select>
+      </div>
+    </div>
+  )
+} 
+
+export default Filters
